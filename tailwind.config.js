d47a2787715903f8bb6ae6e226f361c5e5d0bb9e
Bleu Strnad <bleustrnad@me.com>
2023-07-15 19:06:55 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'loyola': {
+        100: '#ffffff',
+        200: '#DADADA',
+        300: '#67e8f9',
+        400: '#22d3ee',
+        500: '#06b6d4',
+        600: '#0891b2',
+        700: '#8b2a5a',
+        800: '#5d1c3d',
+        900: '#671f43',
+      },
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
